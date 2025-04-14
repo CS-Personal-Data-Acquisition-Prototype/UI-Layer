b7c47b11f13ef006940c400379b98c74f192e72a
statdk <me.statdk@gmail.com>
2025-04-13 22:51:00 -0700

    Main frontend windows drawn-- needs implementation

--- data-display/src/login.rs
+++ data-display/src/login.rs
@@ -1,4 +1,4 @@
-// use eframe::egui;
+use eframe::egui::{self, Color32, RichText};
 
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)]
@@ -10,6 +10,9 @@ pub struct Login {
     // login_token: String,
     logged_in: bool,
     failed_attempts: u8,
+    show_window_sessionhist: bool,
+    show_window_account: bool,
+    show_window_deviceinfo: bool,
 }
 
 impl Default for Login {
@@ -17,14 +20,21 @@ impl Default for Login {
         Self {
             logged_in: false,
             failed_attempts: 0,
+            show_window_sessionhist: false,
+            show_window_account: false,
+            show_window_deviceinfo: false,
         }
     }
 }
 
 impl Login {
-    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+
+        cc.egui_ctx.set_visuals(egui::Visuals {
+            ..Default::default()
+        });
 
         //TODO: Load previous app state if any (must enable the `persistence` feature)
 
@@ -36,6 +46,9 @@ impl Login {
 
         // If successful, show sessions window and set fail count to 0
         self.logged_in = true;
+        self.show_window_sessionhist = true;
+        self.show_window_account = true;
+        self.show_window_deviceinfo = true;
 
         // If fail, increment fail count
     }
@@ -43,6 +56,9 @@ impl Login {
     fn logout(&mut self) {
         // Clear stored user and hide sessions window (should sessions window cascade?)
         self.logged_in = false;
+        self.show_window_sessionhist = false;
+        self.show_window_account = false;
+        self.show_window_deviceinfo = false;
     }
 
     fn show_login_entry(&mut self, ui: &mut eframe::egui::Ui) {
@@ -75,10 +91,89 @@ impl Login {
             Login::logout(self);
         }
     }
+
+    fn show_session_history(&mut self, ui: &mut eframe::egui::Ui) {
+        if ui.button("New Session").clicked() {}
+        egui::Grid::new("session_history_grid").show(ui, |ui| {
+            ui.label(RichText::new("Timestamp").strong());
+            ui.label(RichText::new("Duration").strong());
+            ui.label(RichText::new("Actions").strong());
+            ui.end_row();
+
+            ui.label("...");
+            ui.label("...");
+            if ui.link("Download").clicked() {}
+            if ui.link("Modify").clicked() {}
+            if ui.link("View").clicked() {}
+            ui.end_row();
+        });
+    }
+    fn show_account_info(&mut self, ui: &mut eframe::egui::Ui) {
+        ui.label("User: PLACEHOLDER");
+        ui.label("Email: EXAMPLE@DOMAIN.COM");
+        if ui.link("Change Password").clicked() {
+            // TODO: Implement
+        }
+        if ui.link("Regenerate Device Keys").clicked() {
+            // TODO: Implement
+        }
+        if ui.link("Appearance Settings").clicked() {
+
+            // TODO: Implement
+        }
+    }
+    fn show_device_info(&mut self, ui: &mut eframe::egui::Ui) {
+        // Connection Info Section
+        ui.label(RichText::new("Connection").heading().underline());
+        ui.label(RichText::new("OFFLINE").color(Color32::RED));
+        if ui.link("Settings").clicked() {
+            // TODO: Implement
+        }
+        ui.group(|ui| {
+            ui.label("Type:");
+            ui.horizontal_wrapped(|ui| {
+                ui.group(|ui| {
+                    ui.label(RichText::new("DIRECT").color(Color32::RED));
+                    ui.label("|");
+                    ui.label(RichText::new("PROXY").color(Color32::RED));
+                });
+            });
+            ui.label("IPv4: ---");
+            ui.label("IPv6: ---");
+            ui.label("Cellular Strength: ---");
+        });
+        ui.add_space(5.0);
+
+        // Hardware info section
+        ui.label(RichText::new("Hardware").heading().underline());
+        ui.group(|ui| {
+            ui.label("CPU Temperature: ---");
+            ui.label("CPU Load: ---");
+            ui.label("Battery Temperature: ---");
+            ui.label("Battery Remaining Capacity: ---");
+        });
+    }
 }
 
 impl eframe::App for Login {
     fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::top("top_bar").show(ctx, |ui| {
+            ui.vertical_centered(|ui| {
+                ui.label(
+                    egui::RichText::new("CS.026 Personal Data Acquision Project 24-25").heading(),
+                );
+            });
+        });
+
+        egui::TopBottomPanel::bottom("bottom_bar").show(ctx, |ui| {
+            ui.label(
+                egui::RichText::new(
+                    "Copyright © 2025 CS 46X Personal Data Acquisition Prototype Group ",
+                )
+                .small(),
+            );
+        });
+
         eframe::egui::Window::new("Login Manager").show(ctx, |ui| {
             if !self.logged_in {
                 Login::show_login_entry(self, ui);
@@ -86,6 +181,24 @@ impl eframe::App for Login {
                 Login::show_logged_in(self, ui);
             }
         });
+
+        if self.show_window_sessionhist {
+            eframe::egui::Window::new("Sessions").show(ctx, |ui| {
+                Login::show_session_history(self, ui);
+            });
+        }
+
+        if self.show_window_sessionhist {
+            eframe::egui::Window::new("Account").show(ctx, |ui| {
+                Login::show_account_info(self, ui);
+            });
+        }
+
+        if self.show_window_sessionhist {
+            eframe::egui::Window::new("Device").show(ctx, |ui| {
+                Login::show_device_info(self, ui);
+            });
+        }
     }
 
     // TODO:
